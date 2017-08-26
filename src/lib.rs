2e0ea4554b372788a3946b85d560354fdbad6a7f
Cooper Paul EdenDay <cedenday@protonmail.com>
2017-08-26 08:18:13 -0400

    Added example.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+//! # Example
+//!
+//! ```rust
+//! # extern crate pkcs7;
+//! const BLOCK_SIZE: usize = 16;
+//!
+//! let un_padded = b"This is a test.";
+//!
+//! let mut buffer = un_padded.to_vec();
+//! pkcs7::pad(&mut buffer, BLOCK_SIZE as u8);
+//!
+//! assert_eq!(buffer.len() % BLOCK_SIZE, 0);
+//!
+//! pkcs7::un_pad(&mut buffer);
+//!
+//! assert_eq!(buffer.as_slice(), un_padded);
+//! ```
+
 use std::iter::repeat;
 
 pub fn pad(buffer: &mut Vec<u8>, block_size: u8) {
