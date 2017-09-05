e475fba008cc05d08c7522b12010fe4b61b0a6f1
Cooper Paul EdenDay <cedenday@protonmail.com>
2017-09-04 22:42:22 -0400

    Rewrite by @cedenday

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,33 @@
-pub fn pad(vec: &mut Vec<u8>, block_size: usize) -> &mut Vec<u8> {
-  let remainder = vec.len() % block_size;
-  let mut padding_size = block_size - remainder;
-  let padding_byte = padding_size as u8;
+//! # Example
+//!
+//! ```rust
+//! # extern crate pkcs7;
+//! const BLOCK_SIZE: usize = 16;
+//!
+//! let un_padded = b"This is a test.";
+//!
+//! let mut buffer = un_padded.to_vec();
+//! pkcs7::pad(&mut buffer, BLOCK_SIZE as u8);
+//!
+//! assert_eq!(buffer.len() % BLOCK_SIZE, 0);
+//!
+//! pkcs7::un_pad(&mut buffer);
+//!
+//! assert_eq!(buffer.as_slice(), un_padded);
+//! ```
 
-  if remainder > 0 {
-    while padding_size > 0 {
-      vec.push(padding_byte);
-      padding_size -= 1;
-    }
-  }
+use std::iter::repeat;
 
-  return vec
+pub fn pad(buffer: &mut Vec<u8>, block_size: u8) {
+    let block_size = block_size as usize;
+
+    let padding_size = block_size - (buffer.len() % block_size);
+    buffer.extend(repeat(padding_size as u8).take(padding_size));
 }
 
-
-#[cfg(test)]
-mod tests {
-
-  use super::pad;
-
-  #[test]
-  fn pkcs7_pads_short_input() {
-    let mut actual: Vec<u8> = vec![8, 3, 4, 11, 4];
-    let block_size = 8;
-    pad(&mut actual, block_size);
-
-    let expected: Vec<u8> = vec![8, 3, 4, 11, 4, 3, 3, 3];
-    assert_eq!(actual, expected);
-  }
-
-  #[test]
-  fn pkcs7_doesnt_pad_perfect_length_input() {
-    let mut actual: Vec<u8> = vec![0, 1, 2, 3];
-    pad(&mut actual, 4);
-
-    let expected: Vec<u8> = vec![0, 1, 2, 3];
-    assert_eq!(actual, expected);
-  }
-
+pub fn un_pad(buffer: &mut Vec<u8>) {
+    if let Some(&pad_len) = buffer.last() {
+        let len = buffer.len();
+        buffer.truncate(len - pad_len as usize);
+    }
 }
